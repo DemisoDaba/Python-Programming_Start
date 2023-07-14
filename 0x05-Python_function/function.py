5c75517dbe49fc913ac115a84922d64181fcb63a
DemisoDaba <demo.nkmt1@gmail.com>
2023-07-13 23:27:59 -0700

    Function

---
+++ 0x05-Python_function/function.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 14 09:26:46 2023
+
+@author: Demiso
+"""
+
+def sum_two_numbers(a,b):
+    sum = a+b
+    return sum
+result = sum_two_numbers(3, 5)
+print(result)
